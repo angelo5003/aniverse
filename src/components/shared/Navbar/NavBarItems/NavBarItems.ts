7f70e2aedfa1f3f5816032827589f0f22d75da4a
Tony5300 <tony_tolen@ziggo.nl>
2025-10-02 17:31:03 +0200

    first commit from navbar branch.

---
+++ src/components/shared/Navbar/NavBarItems/NavBarItems.ts
@@ -0,0 +1,25 @@
+import { Home, Search, Bookmark, User, Bell } from "lucide-react";
+
+export const NavBarItems = [
+  {
+    label: "Home",
+    href: "/",
+    ariaLabel: "Go to homepage",
+  },
+  {
+    label: "Bookmark",
+    href: "/bookmark",
+    ariaLabel: "Go to bookmark page",
+  },
+
+  {
+    label: "Notifications",
+    href: "/notifications",
+    ariaLabel: "Go to notifications",
+  },
+  {
+    label: "Account",
+    href: "/account",
+    ariaLabel: "View your account settings",
+  },
+];
