b8904ada87258125b9b7037ad25aec514b0cc220
Tony5300 <tony_tolen@ziggo.nl>
2025-10-02 17:31:03 +0200

    first commit from navbar branch.

--- src/components/shared/Navbar/NavBar.tsx
+++ src/components/shared/Navbar/NavBar.tsx
@@ -2,11 +2,18 @@
 
 import React from "react";
 import { NavBarContainer } from "./NavBar.styled";
+import { Link as ChakraLink } from "@chakra-ui/react";
+import Link from "next/link";
+import { NavBarItems } from "./NavBarItems/NavBarItems";
 
 const NavBar: React.FC = () => {
   return (
-    <NavBarContainer>
-      <h1>Navbar</h1>
+    <NavBarContainer as="nav">
+      {NavBarItems.map((navLink) => (
+        <ChakraLink asChild key={navLink.href}>
+          <Link href={navLink.href}>{navLink.label}</Link>
+        </ChakraLink>
+      ))}
     </NavBarContainer>
   );
 };
