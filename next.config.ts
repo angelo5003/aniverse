7326261f3ef5a0de377df5759d7e6dea6767a7be
Tony5300 <tony_tolen@ziggo.nl>
2025-10-07 17:48:47 +0200

    Added a svg pack to make sure I can import svg's as react components.

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,13 @@ const nextConfig: NextConfig = {
   experimental: {
     optimizePackageImports: ["@chakra-ui/react"],
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
